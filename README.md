759031cc821cd83c6f307fe4c261d71004f2f74a
Aris Ongaro <mail@arisongaro.it>
2022-08-26 12:13:28 +0200

    Adapt for multi-installations

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-# Boilerplate for nginx with Let’s Encrypt on docker-compose
-
-> This repository is accompanied by a [step-by-step guide on how to
-set up nginx and Let’s Encrypt with Docker](https://medium.com/@pentacent/nginx-and-lets-encrypt-with-docker-in-less-than-5-minutes-b4b8a60d3a71).
+# Boilerplate for nginx with Let’s Encrypt on Docker Compose
 
 `init-letsencrypt.sh` fetches and ensures the renewal of a Let’s
 Encrypt certificate for one or multiple domains in a docker-compose
@@ -12,11 +9,11 @@ application.
 ## Installation
 1. [Install docker-compose](https://docs.docker.com/compose/install/#install-compose).
 
-2. Clone this repository: `git clone https://github.com/wmnnd/nginx-certbot.git .`
+2. Clone the repository.
 
 3. Modify configuration:
-- Add domains and email addresses to init-letsencrypt.sh
-- Replace all occurrences of example.org with primary domain (the first one you added to init-letsencrypt.sh) in data/nginx/app.conf
+- Add domains and email addresses to domains.sh (see domains.sh.example).
+- Add Nginx configuration for all domains in data/nginx/$domain.conf (see data/nginx/app.conf.example).
 
 4. Run the init script:
 
@@ -25,9 +22,6 @@ application.
 5. Run the server:
 
         docker-compose up
-
-## Got questions?
-Feel free to post questions in the comment section of the [accompanying guide](https://medium.com/@pentacent/nginx-and-lets-encrypt-with-docker-in-less-than-5-minutes-b4b8a60d3a71)
 
 ## License
 All code in this repository is licensed under the terms of the `MIT License`. For further information please refer to the `LICENSE` file.
--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,36 +1,52 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
+docker compose -v > /dev/null 2>&1 || {
+  echo "Docker compose is not installed. Please install it first."
+  exit 1
+}
+
+source ./domains.sh
+
+if [ -z "${domains}" ]; then
+  echo "domains variable not defined. Please define it in domains.sh"
   exit 1
 fi
 
-domains=(example.org www.example.org)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
-if [ -d "$data_path" ]; then
+if [ -z "${STAGING}" ]; then
+  STAGING=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+fi
+
+if [ -d "${data_path}" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
 fi
 
+if [ -z "${EMAIL}" ]; then
+    read -p "Please type the email address to use for Let's Encrypt account: " EMAIL
+    echo "Using ${EMAIL} as Let's Encrypt account"
+    read -p "Correct? (y/N) " decision
+    if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+        exit
+    fi
+fi
 
-if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+if [ ! -e "${data_path}/conf/options-ssl-nginx.conf" ] || [ ! -e "${data_path}/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
-  mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  mkdir -p "${data_path}/conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "${data_path}/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "${data_path}/conf/ssl-dhparams.pem"
   echo
 fi
 
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
+mkdir -p "${data_path}/conf/live/$domains"
+docker compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -39,11 +55,11 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
+docker compose up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
+docker compose run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -58,18 +74,18 @@ for domain in "${domains[@]}"; do
 done
 
 # Select appropriate email arg
-case "$email" in
+case "${EMAIL}" in
   "") email_arg="--register-unsafely-without-email" ;;
-  *) email_arg="--email $email" ;;
+  *) email_arg="--email ${EMAIL}" ;;
 esac
 
 # Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
+if [ ${STAGING} != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
+docker compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
-    $email_arg \
+    ${email_arg} \
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
@@ -77,4 +93,4 @@ docker-compose run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+docker compose exec nginx nginx -s reload
